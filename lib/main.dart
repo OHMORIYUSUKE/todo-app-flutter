dcf1ecdbe0f819e8e42838060a57aa240a7e38ae
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2023-09-15 00:34:11 +0900

    feat: タスクをカードで表示機能を実装

--- lib/main.dart
+++ lib/main.dart
@@ -26,8 +26,34 @@ class TodoListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(
-        child: Text('リスト一覧画面'),
+      // AppBarを表示し、タイトルも設定
+      appBar: AppBar(
+        title: const Text('リスト一覧'),
+      ),
+      body: ListView(
+        children: const <Widget>[
+          // CardとListTileを使い、簡単に整ったUIを作成
+          Card(
+            child: ListTile(
+              title: Text('ニンジンを買う'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Text('タマネギを買う'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Text('ジャガイモを買う'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Text('カレールーを買う'),
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
