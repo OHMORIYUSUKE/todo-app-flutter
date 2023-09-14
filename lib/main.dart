20a9db94d4b1a26ff35d2c9f52f287b74efec193
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2023-09-15 01:09:09 +0900

    feat: タスク追加画面から状態をTOPページに渡す処理を実装

--- lib/main.dart
+++ lib/main.dart
@@ -56,23 +56,37 @@ class TodoListPage extends StatelessWidget {
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
+        onPressed: () async {
           // "push"で新規画面に遷移
-          Navigator.of(context).push(
+          // リスト追加画面から渡される値を受け取る
+          final newListText = await Navigator.of(context).push(
             MaterialPageRoute(builder: (context) {
               // 遷移先の画面としてリスト追加画面を指定
               return TodoAddPage();
             }),
           );
+          print(newListText);
+          if (newListText != null) {
+            // キャンセルした場合は newListText が null となるので注意
+          }
         },
-        child: const Icon(Icons.add),
+        child: Icon(Icons.add),
       ),
     );
   }
 }
 
+// 状態を持つ
+class TodoAddPage extends StatefulWidget {
+  @override
+  _TodoAddPageState createState() => _TodoAddPageState();
+}
+
 // リスト追加画面用Widget
-class TodoAddPage extends StatelessWidget {
+class _TodoAddPageState extends State<TodoAddPage> {
+  // 入力されたテキストをデータとして持つ
+  String _text = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -85,15 +99,32 @@ class TodoAddPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
+              // 入力されたテキストを表示
+              Text(_text, style: TextStyle(color: Colors.blue)),
+              const SizedBox(height: 8),
               // テキスト入力
-              const TextField(),
+              TextField(
+                // 入力されたテキストの値を受け取る（valueが入力されたテキスト）
+                onChanged: (String value) {
+                  // データが変更したことを知らせる（画面を更新する）
+                  setState(() {
+                    // データを変更
+                    _text = value;
+                  });
+                },
+              ),
               const SizedBox(height: 8),
               Container(
                 // 横幅いっぱいに広げる
                 width: double.infinity,
                 // リスト追加ボタン
+                // リスト追加ボタン
                 child: ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    // "pop"で前の画面に戻る
+                    // "pop"の引数から前の画面にデータを渡す
+                    Navigator.of(context).pop(_text);
+                  },
                   child: const Text('リスト追加', style: TextStyle(color: Colors.white)),
                 ),
               ),
