790edf64dd691a5d3b34dc8c758a784e3f2bfd2f
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2023-09-23 15:44:46 +0900

    feat: 画面遷移まで実装

--- lib/main.dart
+++ lib/main.dart
@@ -2,81 +2,228 @@ import 'package:flutter/material.dart';
 
 void main() {
   // 最初に表示するWidget
-  runApp(MyTodoApp());
+  runApp(PokeApp());
 }
 
-class MyTodoApp extends StatelessWidget {
+class PokeApp extends StatelessWidget {
+  ThemeMode mode = ThemeMode.system;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       // アプリ名
       title: 'My Todo App',
-      theme: ThemeData(
-        // テーマカラー
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: mode,
       // リスト一覧画面を表示
-      home: TodoListPage(),
+      home: TopPage(),
     );
   }
 }
 
-// リスト一覧画面用Widget
-class TodoListPage extends StatefulWidget {
+// カード
+class PokeListItem extends StatelessWidget {
+  const PokeListItem({Key? key, required this.index}) : super(key: key);
+  final int index;
+
   @override
-  _TodoListPageState createState() => _TodoListPageState();
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Container(
+        width: 80,
+        decoration: BoxDecoration(
+          color: Colors.yellow.withOpacity(.5),
+          borderRadius: BorderRadius.circular(10),
+          image: const DecorationImage(
+            fit: BoxFit.fitWidth,
+            image: NetworkImage(
+              "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+            ),
+          ),
+        ),
+      ),
+      title: Text(
+        'Pikachu $index',
+        style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+      ),
+      subtitle: const Text(
+        '⚡️electric',
+      ),
+      trailing: const Icon(Icons.navigate_next),
+      onTap: () => {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (BuildContext context) => PokeDetail(),
+          ),
+        ),
+      },
+    );
+  }
 }
 
-// リスト一覧画面用Widget
-class _TodoListPageState extends State<TodoListPage> {
-  // Todoリストのデータ
-  List<String> todoList = [];
+// トップページ
+class TopPage extends StatefulWidget {
+  const TopPage({Key? key}) : super(key: key);
+  @override
+  _TopPageState createState() => _TopPageState();
+}
+
+// カードのリスト
+class PokeList extends StatelessWidget {
+  const PokeList({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+      itemCount: 1010,
+      itemBuilder: (context, index) => PokeListItem(index: index),
+    );
+  }
+}
+
+// テーマ設定画面
+class ThemeModeSelectionPage extends StatefulWidget {
+  const ThemeModeSelectionPage({Key? key}) : super(key: key);
+  @override
+  _ThemeModeSelectionPageState createState() => _ThemeModeSelectionPageState();
+}
+
+class _ThemeModeSelectionPageState extends State<ThemeModeSelectionPage> {
+  ThemeMode _current = ThemeMode.system;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // AppBarを表示し、タイトルも設定
-      appBar: AppBar(
-        title: const Text('リスト一覧'),
-      ),
-      body: ListView.builder(
-        itemCount: todoList.length,
-        itemBuilder: (context, index) {
-          return Card(
-            child: ListTile(
-              title: Text(todoList[index]),
-              trailing: IconButton(
-                icon: Icon(Icons.delete), // ゴミ箱アイコン
-                onPressed: () {
-                  // タスクを削除
-                  setState(() {
-                    todoList.removeAt(index);
-                  });
-                },
+      body: SafeArea(
+        child: Column(
+          children: [
+            ListTile(
+              leading: IconButton(
+                icon: const Icon(Icons.arrow_back),
+                onPressed: () => Navigator.pop(context),
               ),
             ),
-          );
-        },
+            RadioListTile<ThemeMode>(
+              value: ThemeMode.system,
+              groupValue: ThemeMode.system,
+              title: const Text('System'),
+              onChanged: (val) => {setState(() => _current = val!)},
+            ),
+            RadioListTile<ThemeMode>(
+              value: ThemeMode.dark,
+              groupValue: ThemeMode.system,
+              title: const Text('Dark'),
+              onChanged: (val) => {setState(() => _current = val!)},
+            ),
+            RadioListTile<ThemeMode>(
+              value: ThemeMode.light,
+              groupValue: ThemeMode.system,
+              title: const Text('Light'),
+              onChanged: (val) => {setState(() => _current = val!)},
+            ),
+          ],
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          // "push"で新規画面に遷移
-          // リスト追加画面から渡される値を受け取る
-          final newListText = await Navigator.of(context).push(
-            MaterialPageRoute(builder: (context) {
-              // 遷移先の画面としてリスト追加画面を指定
-              return TodoAddPage();
-            }),
-          );
-          if (newListText != null) {
-            // キャンセルした場合は newListText が null となるので注意
-            setState(() {
-              // リスト追加
-              todoList.add(newListText);
-            });
-          }
+    );
+  }
+}
+
+// 設定
+class Settings extends StatelessWidget {
+  const Settings({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        ListTile(
+          leading: const Icon(Icons.lightbulb),
+          title: const Text('Dark/Light Mode'),
+          onTap: () => {
+            Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => const ThemeModeSelectionPage(),
+            )),
+          },
+        ),
+      ],
+    );
+  }
+}
+
+// トップページ(状態)
+class _TopPageState extends State<TopPage> {
+  int currentbnb = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: currentbnb == 0 ? const PokeList() : const Settings(),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) => {
+          setState(
+                () => currentbnb = index,
+          )
         },
-        child: Icon(Icons.add),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'settings',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// 詳細
+class PokeDetail extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Stack(
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(32),
+                  child: Image.network(
+                    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+                    height: 100,
+                    width: 100,
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  child: const Text(
+                    'No.25',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const Text(
+              'pikachu',
+              style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            ),
+            Chip(
+              label: Text('electric', style: TextStyle(
+                  color: Colors.yellow.computeLuminance() > 0.5
+                      ? Colors.black
+                      : Colors.white),),
+              backgroundColor: Colors.yellow,
+            ),
+          ],
+        ),
       ),
     );
   }
